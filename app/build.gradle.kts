0b2a363af76151fa41e49d0b781bb6c5afc4a595
Daniel Manrique <dmanluc91@gmail.com>
2021-02-15 12:19:32 +0100

    Convert groovy syntax to kotlin DSL in all gradle files

---
+++ app/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    compileSdkVersion(AppConfig.compileSdk)
+    buildToolsVersion(AppConfig.buildToolsVersion)
+
+    defaultConfig {
+        applicationId = "dev.dmanluc.openbankmobiletest"
+        minSdkVersion(AppConfig.minSdk)
+        targetSdkVersion(AppConfig.targetSdk)
+        versionCode = AppConfig.versionCode
+        versionName = AppConfig.versionName
+
+        testInstrumentationRunner(AppConfig.androidTestInstrumentation)
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(AppDependencies.appLibraries)
+    testImplementation(AppDependencies.testLibraries)
+    androidTestImplementation(AppDependencies.androidTestLibraries)
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,16 @@
+object Versions {
+    //Core
+    const val gradle = "4.1.2"
+    const val kotlin = "1.4.30"
+
+    // Android
+    val coreKtx = "1.3.2"
+    val appcompat = "1.2.0"
+    val constraintLayout = "2.0.4"
+    val materialDesign = "1.3.0"
+
+    // Testing
+    val junit = "4.13.1"
+    val extJunit = "1.1.2"
+    val espresso = "3.2.0"
+}
